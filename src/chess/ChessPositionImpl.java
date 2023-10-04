2c3a58a8fc036ee085f37a8365664e38c1e2c006
Jeenu Lee <jeenulee@jeenus-air.lan>
2023-10-04 15:54:32 -0600

    implementation

---
+++ src/chess/ChessPositionImpl.java
@@ -0,0 +1,13 @@
+package chess;
+
+public class ChessPositionImpl implements ChessPosition {
+    @Override
+    public int getRow() {
+        return 0;
+    }
+
+    @Override
+    public int getColumn() {
+        return 0;
+    }
+}
